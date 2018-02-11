caaaa078974a4ca0be0a34ae9822b1a2c5d99396
Kyle Helle <kyle@coinme.com>
2018-02-11 10:48:10 -0800

    initial commit

---
+++ dist/src/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+var server = require('net').createServer();
+var counter = 0;
+var sockets = {};
+
+server.on('connection', function (socket) {
+    socket.id = counter++;
+    sockets[socket.id] = socket;
+
+    console.log('Client connencted.');
+    socket.write('Welcome new client.\n');
+
+    socket.on('data', function (data) {
+        Object.entries(sockets).forEach(function (_ref) {
+            var _ref2 = _slicedToArray(_ref, 2),
+                cs = _ref2[1];
+
+            cs.write(socket.id + ': ');
+            cs.write(data);
+        });
+    });
+
+    socket.on('end', function () {
+        delete sockets[socket.id];
+        console.log('Client disconnected.');
+    });
+});
+
+server.listen(8000, function () {
+    return console.log('Server bound.');
+});
---
+++ src/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const server = require('net').createServer();
+let counter = 0;
+let sockets = {};
+
+server.on('connection', socket => {
+    socket.id = counter++;
+    sockets[socket.id] = socket;
+
+    console.log('Client connencted.');
+    socket.write('Welcome new client.\n');
+
+    socket.on('data', data => {
+        Object.entries(sockets).forEach(([, cs]) => {
+            cs.write(`${socket.id}: `);
+            cs.write(data);
+        });
+    });
+
+    socket.on('end', () => {
+        delete sockets[socket.id];
+        console.log('Client disconnected.');
+    });
+});
+
+server.listen(8000, () => console.log('Server bound.'));
+
+
